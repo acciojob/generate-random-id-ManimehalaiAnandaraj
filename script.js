41cd71acf7bbcaf1c7d6be1aa25b02db435bc730
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-21 11:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function makeid(l) {
   // write your code here
-}
+	var res="";
+		var char_list="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		for(let i=0;i<l;i++)
+			{
+				
+				res +=char_list.charAt(Math.floor
+									  (Math.random()*char_list.length))
+			}
+		return res;
+
+	}
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
